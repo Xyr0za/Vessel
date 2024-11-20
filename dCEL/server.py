cf8ba5a1ad926142ed60ba319d3ed52bff64d68b
Euan Baldwin <102750293+Xyr0za@users.noreply.github.com>
2024-11-20 23:03:30 +0000

    Update and rename module.py to server.py

--- dCEL/server.py
+++ dCEL/server.py
@@ -1,10 +1,6 @@
 from dCEL.master import MasterNode
 from dCEL.coordinator import Coordinator
 
-import hashlib
-import datetime
-
-
 if __name__ == '__main__':
     coordinator = Coordinator(current_iter=0, batch_size=1000, nodes={*()})
     master = MasterNode(
